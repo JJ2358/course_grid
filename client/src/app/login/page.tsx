0220ab048ddb6022a8a92e156f28483a52d052f6
Christian Marcelo <christian.marcelo.castro@gmail.com>
2024-02-06 18:33:48 -0400

    adding feedback messages

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,10 +1,15 @@
+'use client';
+
 import { useFormState } from 'react-dom';
 
 import Link from "next/link";
 
+import { loginUser } from '@/tools/actions';
 
 
 export default function LoginPage() {
+
+    const [formState, action] = useFormState(loginUser, { feedback: '' });
 
     return (
         <div className="h-screen">
@@ -16,7 +21,7 @@ export default function LoginPage() {
             </nav>
 
             <main className="flex justify-center align-middle h-[85vh] bg-slate-800">
-                <form className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-200 dark:border-gray-700 self-center" method="POST">
+                <form className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-200 dark:border-gray-700 self-center" action={action}>
                     <div className="space-y-12">
                         <div className="border-b border-gray-900/10 pb-12">
                             <h2 className="text-3xl font-bold leading-7 text-gray-900">Login</h2>
@@ -25,19 +30,24 @@ export default function LoginPage() {
                                 <div className="sm:col-span-3">
                                     <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email</label>
                                     <div className="mt-2">
-                                        <input type="email" name="email" id="email" autoComplete="Email address" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
+                                        <input type="email" name="email" id="email" autoComplete="Email address" className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
                                     </div>
+
+                                    {typeof formState.emailMessage === 'string' ? <span className='text-red-500'>{formState.emailMessage} </span> : ''}
                                 </div>
 
                                 <div className="sm:col-span-4">
                                     <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
                                     <div className="mt-2">
-                                        <input id="password" name="password" type="password" autoComplete="Password" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
+                                        <input id="password" name="password" type="password" autoComplete="Password" className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
                                     </div>
+
+                                    {typeof formState.passMessage === 'string' ? <span className=' text-red-500'>{formState.passMessage} </span> : ''}
                                 </div>
 
                             </div>
                         </div>
+                        {typeof formState.feedback === 'string' ? <span className='text-red-500'>{formState.feedback} </span> : ''}
                     </div>
 
                     <div className="mt-6 flex items-center flex-col gap-y-6">
