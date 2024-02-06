0fd17748509357d567d41ad3632860d0006dbe33
Christian Marcelo <christian.marcelo.castro@gmail.com>
2024-02-06 10:50:02 -0400

    change post with id to just plain post

--- client/src/app/api/account/post/[id].ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse }  from "next";
-import { createAccount } from "@/tools/DataManager";
-
-export default async function handler( request:NextApiRequest, response:NextApiResponse ) {
-    // response.send("request received!");
-    if (request.method == "POST") {
-        await createAccount(request, response);
-    }
-}
